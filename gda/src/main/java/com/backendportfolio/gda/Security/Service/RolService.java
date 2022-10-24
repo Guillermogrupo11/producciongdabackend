11f7c30fd5ab51d93ddcc7ebbc647311e952588d
Guillermo <guillermo.arena@gmail.com>
2022-10-24 20:09:03 -0300

    SecurityService

---
+++ gda/src/main/java/com/backendportfolio/gda/Security/Service/RolService.java
@@ -0,0 +1,25 @@
+
+package com.backendportfolio.gda.Security.Service;
+
+import com.backendportfolio.gda.Security.Entity.Rol;
+import com.backendportfolio.gda.Security.Enums.RolNombre;
+import com.backendportfolio.gda.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
