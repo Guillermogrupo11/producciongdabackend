4111b55cfad6347fefcd4a749b7ede0c261d97ff
Guillermo <guillermo.arena@gmail.com>
2022-10-24 14:32:38 -0300

    creando proyecto

---
+++ gda/src/test/java/com/backendportfolio/gda/GdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendportfolio.gda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
