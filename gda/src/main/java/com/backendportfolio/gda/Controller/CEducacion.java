075cb65d693d7079ec81dec82e3c18a992603d89
Guillermo <guillermo.arena@gmail.com>
2022-10-24 23:46:13 -0300

    arreglando controllers

--- gda/src/main/java/com/backendportfolio/gda/Controller/CEducacion.java
+++ gda/src/main/java/com/backendportfolio/gda/Controller/CEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://mgbfrontend.web.app")
+@CrossOrigin(origins = "https://gdafrontendargprog.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- gda/src/main/java/com/backendportfolio/gda/Controller/CExperiencia.java
+++ gda/src/main/java/com/backendportfolio/gda/Controller/CExperiencia.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/explab")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://mgbfrontend.web.app")
+@CrossOrigin(origins = "https://gdafrontendargprog.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
